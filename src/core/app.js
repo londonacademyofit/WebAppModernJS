fa5c02a0b6d8f2930dc0d6d02f6b7b85dbd09861
Louise Swift <louise@swiftfeather.com>
2014-10-01 14:01:48 +0100

    New factories (userAuth, authWatch)

--- src/core/app.js
+++ src/core/app.js
@@ -16,7 +16,10 @@ var appConfig = {
 angular.module('PartyPlannerApp', [
 
 	'ngRoute',
-	'firebase'
+	'firebase',
+	'angus.templates.app',
+	'user-profile',
+	'user-events'
 
 ]);
 
@@ -38,52 +41,19 @@ angular.module('PartyPlannerApp').config(['$routeProvider', '$locationProvider',
 
 }]);
 
-// AppCtrl provides app-wide functionality, e.g. user authentication
+// this factory connects us to our Firebase for authentication tasks
 
-angular.module('PartyPlannerApp').controller('AppCtrl', ['$scope', '$location', '$firebase', '$firebaseSimpleLogin', function($scope, $location, $firebase, $firebaseSimpleLogin) {
-
-	$scope.test = 'Hello world!';
-
-	// set up Firebase's Simple Login service ready to respond to .login() and .logout() calls
+angular.module('PartyPlannerApp').factory('userAuth', ['$firebaseSimpleLogin', function($firebaseSimpleLogin) {
 
 	var firebaseRef = new Firebase(appConfig.firebaseUrl);
 
-	var authClient = $firebaseSimpleLogin(firebaseRef, function(error, user) {
+	return $firebaseSimpleLogin(firebaseRef);
 
-		if (error) {
+}]);
 
-			console.log('error:', error);
+// this factory watches for changes in the user's authentication status
 
-			alert('There was an error with the login process. Please try again.')
-
-		}
-
-		if (user) {
-		
-			console.log('user logged in:', user);
-
-			// if the user is logged in, set/update their Firebase entry
-
-			firebaseRef.child('users').child(user.uid).set({
-
-				// NOTE: come back later to add a user's profile photo to their Firebase entry
-				// as per https://dev.twitter.com/rest/reference/get/users/show
-
-				displayName: user.displayName,
-				provider: user.provider,
-				provider_id: user.id
-			
-			});		
-
-		} else {
-
-			console.log('user not logged in');
-
-		}
-
-	});
-
-	// watch for any user authentication changes and show the right buttons accordingly
+angular.module('PartyPlannerApp').factory('authWatch', ['$location', 'userAuth', function($location, $userAuth) {
 
 	var authRef = new Firebase(appConfig.firebaseUrl + '/.info/authenticated');
 
@@ -95,11 +65,7 @@ angular.module('PartyPlannerApp').controller('AppCtrl', ['$scope', '$location', 
 
 			$('.logout').show();
 
-			$('.login .loading-gif').show();
-
-			$('.login input[type="button"]').hide();
-
-			$location.url('/');
+			$location.path('/my-profile');
 
 		} else {
 
@@ -107,35 +73,98 @@ angular.module('PartyPlannerApp').controller('AppCtrl', ['$scope', '$location', 
 
 			$('.logout').hide();
 
-			$('.login .loading-gif').hide();
-
-			$('.login input[type="button"]').show();
-
-			$location.url('/');
+			$location.path('/');
 
 		}
 
 	});
 
-	// when the .login-twitter button is clicked, log the user in with their Twitter account
-	// (detailed instructions: https://www.firebase.com/docs/web/guide/simple-login/twitter.html)
-
-	$('input[type="button"].login-twitter').click(function() {
-		
-		authClient.$login('twitter');
-
-		console.log('Logged in with Twitter');
-
-	});
-
-	// the logout button should log the user out
-
-	$('.logout input[type="button"]').click(function() {
-
-		authClient.$logout();
-
-		console.log('user logged out');
-
-	});
-
 }]);
+
+// HomeCtrl and its routing are set up here
+
+angular.module('PartyPlannerApp')
+
+	.config(['$routeProvider', function($routeProvider) {
+
+	  // fetch the correct HTML template / view for this module
+
+		$routeProvider.when('/', {
+
+		    controller: 'HomeCtrl',
+
+		   	templateUrl: 'homepage/homepage.tpl.html',
+
+		    resolve: {
+
+		    	// controller will not be loaded until $getCurrentUser resolves
+      
+      			currentUser: ['userAuth', function(userAuth) {
+        		
+        			// $getCurrentUser returns a promise so the resolve waits for it to complete
+
+					return userAuth.$getCurrentUser();
+      			
+      			}]
+
+		    }
+
+		})
+
+	}])
+
+	.controller('HeaderCtrl', ['$scope', '$location', 'userAuth', function($scope, $location, userAuth) {
+
+		// the logout button in the header should log the user out
+
+		$('.logout input[type="button"]').click(function() {
+
+			userAuth.$logout();
+
+			console.log('user logged out');
+
+			$location.path('/');
+
+		});
+
+	}])
+
+	.controller('HomeCtrl', ['$scope', '$location', 'userAuth', 'authWatch', 'currentUser', function($scope, $location, userAuth, authWatch, currentUser) {
+
+		console.log(' > HomeCtrl');
+
+		// when the .login-twitter button is clicked, log the user in with their Twitter account
+		// (detailed instructions: https://www.firebase.com/docs/web/guide/simple-login/twitter.html)
+
+		$('input[type="button"].login-twitter').click(function() {
+			
+			userAuth.$login('twitter').then(function(user) {
+
+				// when a user logs in, set/update their Firebase entry
+
+				var firebaseRef = new Firebase(appConfig.firebaseUrl);
+
+				firebaseRef.child('users').child(user.uid).set({
+
+					displayName: user.displayName,
+					provider: user.provider,
+					provider_id: user.id,
+					picture: user.thirdPartyUserData.profile_image_url
+				
+				});
+
+				console.log('Logged in with Twitter');
+
+			});
+
+		});
+
+		// if the user is already logged in, send them to their profile page
+
+		if (currentUser) {
+
+			$location.path('/my-profile');
+
+		}
+
+	}]);
